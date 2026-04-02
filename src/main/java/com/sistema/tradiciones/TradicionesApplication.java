f638a6909520107c4355699321eea0b406c42489
PC <PC@CAJA2SUCU>
2026-04-02 16:28:41 -0500

    Primer commit del proyecto Tradiciones

---
+++ src/main/java/com/sistema/tradiciones/TradicionesApplication.java
@@ -0,0 +1,13 @@
+package com.sistema.tradiciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradicionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradicionesApplication.class, args);
+	}
+
+}
